b9613b0ae720f87ed4624ba02de9f800fee70a5c
Aditya <aditya@teebeds.com>
2023-10-04 09:29:18 +0530

    Lazy loading implemented

--- src/app-routing.js
+++ src/app-routing.js
@@ -2,8 +2,11 @@ import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import InvalidRoute from "./containers/invalid-route/InvalidRoute.js";
 import AuthenticationGuard from "./utils/AuthenticationGuard/AuthenticationGuard.js";
 import RoleRouterGuard from "./utils/authenticationGuard/RoleRouteGuard.js";
-import AdminLayout from "./containers/admin/AdminLayout.js";
 import Login from "./containers/login/Login.js";
+import { Suspense, lazy } from "react";
+
+// Lazy Loading
+const AdminLayout = lazy(() => import("./containers/admin/AdminLayout.js"));
 
 const AppRouting = () => {
   return (
@@ -13,8 +16,12 @@ const AppRouting = () => {
         <Route path="login" element={<Login />} />
         <Route element={<AuthenticationGuard />}>
           <Route path="admin" element={<RoleRouterGuard role={"ADMIN"} />}>
-            <Route path="" element={<AdminLayout />}></Route>
-            <Route path="*" element={<AdminLayout />}></Route>
+            <Suspense>
+              <Route path="" element={<AdminLayout />}></Route>
+            </Suspense>
+            <Suspense>
+              <Route path="*" element={<AdminLayout />}></Route>
+            </Suspense>
           </Route>
           <Route path="*" element={<InvalidRoute />} />
           {/* <Route path="seller/*" element={<AdminRouterGuard />}></Route>
